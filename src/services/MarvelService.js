f1d24c6f564461b1a348e576070b90d323d68f70
Alexei Kruk <alexei.kruk.dev@gmail.com>
2024-06-29 19:07:28 +0300

    add charList

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -6,7 +6,7 @@ class MarvelService {
 		let res = await fetch(url);
 
 		if (!res.ok) {
-			throw new Error(`Could not fetch ${url}, status ${res.status}`);
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
 		}
 
 		return await res.json();
@@ -28,11 +28,15 @@ class MarvelService {
 
 	_transformCharacter = char => {
 		return {
+			id: char.id,
 			name: char.name,
-			description: char.description,
+			description: char.description
+				? `${char.description.slice(0, 210)}...`
+				: 'There is no description for this character',
 			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
 			homepage: char.urls[0].url,
 			wiki: char.urls[1].url,
+			comics: char.comics.items,
 		};
 	};
 }
